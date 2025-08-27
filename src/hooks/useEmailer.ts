d0e9683eed22a4b306baf4809767829f06260894
Siby Mathew <siby@sonicdex.io>
2025-08-27 21:27:19 +0530

    design fix

--- src/hooks/useEmailer.ts
+++ src/hooks/useEmailer.ts
@@ -33,6 +33,7 @@ export const useEmailer = () => {
     updateStatus,
     collpaseComposer,
     expandComposer,
+    onClose,
   } = useComposer();
   const { context } = useComposer();
   const { attachmentRef } = useMailBody(ref, context);
@@ -40,13 +41,6 @@ export const useEmailer = () => {
   const connection = useSolanaConnection();
   const { account } = useGetLinkedUsernameById(thread);
   const queryClient = useQueryClient();
-  const sleep = async (delay: number) => {
-    return new Promise((r) => {
-      setTimeout(() => {
-        r(1);
-      }, delay);
-    });
-  };
 
   const IS_FORWARDING = action === MailShareTypes.forward;
   return useMutation({
@@ -57,10 +51,7 @@ export const useEmailer = () => {
       const data = await resolveRecepient({ username: values.to });
       if (!data || !data.address || !from) {
         expandComposer();
-        showToast("Failed to send", {
-          type: "error",
-        });
-        return;
+        throw Error("Unable to resolve recipient address");
       }
       const to = data.address;
 
@@ -76,8 +67,6 @@ export const useEmailer = () => {
 
       if (values && values.files && values.files.length > 0) {
         updateStatus("Uploading files");
-      } else {
-        sleep(1500);
       }
 
       /**
@@ -99,11 +88,15 @@ export const useEmailer = () => {
           });
         });
 
-        const response = await uploadToPinata({
-          data: attachMentFormData,
-        });
-        if (response && response.IpfsHash) {
-          attachmentHash = response.IpfsHash;
+        try {
+          const response = await uploadToPinata({
+            data: attachMentFormData,
+          });
+          if (response && response.IpfsHash) {
+            attachmentHash = response.IpfsHash;
+          }
+        } catch {
+          throw Error("Failed to upload attachments");
         }
       }
 
@@ -142,88 +135,86 @@ export const useEmailer = () => {
 
       const formData = new FormData();
       formData.append("file", textFile);
-
-      const contentId = await uploadToPinata({
-        data: formData,
-      });
-
-      const id = contentId && contentId.IpfsHash;
-      if (!id) {
-        expandComposer();
-        showToast("Failed to send email", {
-          type: "error",
+      let id: any;
+      try {
+        const contentId = await uploadToPinata({
+          data: formData,
         });
+
+        id = contentId && contentId.IpfsHash;
+      } catch {
+        throw Error("Failed to create mail");
+      }
+
+      if (!provider || !program) {
         return;
       }
 
-      try {
-        if (!provider || !program) {
-          return;
-        }
+      updateStatus("Sending mail");
+      const mailAccount = web3.Keypair.generate();
+      const userPublicKey = provider.publicKey;
 
-        updateStatus("Sending mail");
-        const mailAccount = web3.Keypair.generate();
-        const userPublicKey = provider.publicKey;
+      const createMailInstruction = await program.methods
+        .createmail(
+          encryptData(values.subject, cData.iv, key),
+          userPublicKey,
+          to,
+          "salt!",
+          cData.iv,
+          StorageVersion.pinata,
+          ref || "0"
+        )
+        .accounts({
+          mail: mailAccount.publicKey,
+          authority: userPublicKey,
+          mailAccountV2: mailAccountAddress,
+        })
+        .instruction();
 
-        const createMailInstruction = await program.methods
-          .createmail(
-            encryptData(values.subject, cData.iv, key),
-            userPublicKey,
-            to,
-            "salt!",
-            cData.iv,
-            StorageVersion.pinata,
-            ref || "0"
-          )
-          .accounts({
-            mail: mailAccount.publicKey,
-            authority: userPublicKey,
-            mailAccountV2: mailAccountAddress,
-          })
-          .instruction();
+      const updateEmailInstruction = await program.methods
+        .updatemail(id)
+        .accounts({
+          mail: mailAccount.publicKey,
+          authority: userPublicKey,
+        })
+        .instruction();
 
-        const updateEmailInstruction = await program.methods
-          .updatemail(id)
-          .accounts({
-            mail: mailAccount.publicKey,
-            authority: userPublicKey,
-          })
-          .instruction();
+      const transaction = new Transaction().add(
+        createMailInstruction,
+        updateEmailInstruction
+      );
 
-        const transaction = new Transaction().add(
-          createMailInstruction,
-          updateEmailInstruction
-        );
+      const latestBlockhash = await connection.getLatestBlockhash("confirmed");
+      transaction.recentBlockhash = latestBlockhash.blockhash;
+      transaction.feePayer = new PublicKey(
+        wallet?.address?.toString() as string
+      );
+      transaction.partialSign(mailAccount);
 
-        const latestBlockhash =
-          await connection.getLatestBlockhash("confirmed");
-        transaction.recentBlockhash = latestBlockhash.blockhash;
-        transaction.feePayer = new PublicKey(
-          wallet?.address?.toString() as string
-        );
-        transaction.partialSign(mailAccount);
-
-        await sendTransaction({
-          transaction: transaction,
-          connection: connection,
-          uiOptions: {
-            showWalletUIs: !1,
-          },
-        });
-        queryClient.invalidateQueries({ queryKey: [QueryKeys.MAILBOX] });
-        showToast("Mail sent successfully", {
-          type: "success",
-        });
-        close();
-        refetch();
-      } catch {
-        queryClient.invalidateQueries({ queryKey: [QueryKeys.MAILBOX] });
-        refetch();
-        expandComposer();
-        showToast("Failed to send mail", {
-          type: "error",
-        });
-      }
+      await sendTransaction({
+        transaction: transaction,
+        connection: connection,
+        uiOptions: {
+          showWalletUIs: !1,
+        },
+      });
+      queryClient.invalidateQueries({ queryKey: [QueryKeys.MAILBOX] });
+    },
+    onSuccess: () => {
+      onClose();
+      refetch();
+      showToast("Mail sent successfully", {
+        type: "success",
+      });
+    },
+    onError: (e) => {
+      console.log(e);
+      queryClient.invalidateQueries({ queryKey: [QueryKeys.MAILBOX] });
+      refetch();
+      expandComposer();
+      showToast(e && e instanceof Error ? e.message : "Failed to send mail", {
+        type: "error",
+      });
     },
   });
 };
