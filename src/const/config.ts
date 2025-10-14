20a477c0b515ebd7c20f4ee80c94154d93726b36
Siby Mathew <siby@sonicdex.io>
2025-10-14 10:01:30 +0530

    docs url update

--- src/const/config.ts
+++ src/const/config.ts
@@ -17,7 +17,7 @@ export const NO_BALANCE_LABEL = `Insufficient Balance`;
 export const TELEGRAM_URL = `https://t.me/solmailofficial`;
 export const TWITTER_URL = `https://x.com/SolMailOfficial`;
 export const INSTAGRAM_URL = `https://www.instagram.com/solmailofficial/`;
-export const DOCS_URL = `https://solmail-1.gitbook.io/solmail/`;
+export const DOCS_URL = `https://docs.solmail.so/`;
 
 export const PRIVACY_POLICY_LINK =
   "https://elastic-grip-d07.notion.site/Privacy-Policy-25750f9ac71680129ef5e99dc1ac860f";
