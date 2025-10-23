4b93b136c77f9f5fc2e669af8769f6da999366f6
Siby Mathew <siby@sonicdex.io>
2025-10-23 15:18:53 +0530

    code cleanup

--- src/hooks/useGetMailFromContext.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import { MailCardContext } from "src/context/MailCardContext";
-
-export const useGetMailFromContext = () => {
-  const mail = useContext(MailCardContext);
-  return mail;
-};
