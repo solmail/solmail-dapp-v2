1ce1c807524c7fecb9ec5dffe8300b51c21336cf
Siby Mathew <siby@sonicdex.io>
2025-11-04 21:07:43 +0530

    helius config updated

--- src/const/config.ts
+++ src/const/config.ts
@@ -10,7 +10,8 @@ export const MAXIMUM_MAIL_SUBJECT_LENGTH = 80;
 
 export const RPC_ENDPOINT = `${import.meta.env.VITE_SOLMAIL_RPC_ENDPOINT}?api-key=${import.meta.env.VITE_SOLMAIL_RPC_API_KEY}`;
 
-export const HELIUS_API_ENDPOINT = `https://api-devnet.helius.xyz/v0/`;
+export const HELIUS_API_ENDPOINT = import.meta.env
+  .VITE_SOLMAIL_HELIUS_API_ENDPOINT;
 
 export const NO_BALANCE_LABEL = `Insufficient Balance`;
 
