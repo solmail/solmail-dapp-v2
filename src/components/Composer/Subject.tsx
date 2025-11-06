357652eea76831ae7f7a7e22eeb3144b271744f7
Siby Mathew <siby@sonicdex.io>
2025-11-06 10:46:48 +0530

    subject validation

--- src/components/Composer/Subject.tsx
+++ src/components/Composer/Subject.tsx
@@ -1,5 +1,7 @@
 import { Box, Input } from "@chakra-ui/react";
 import { MAXIMUM_MAIL_SUBJECT_LENGTH } from "@const/config";
+import { getByteSize } from "@utils/string/getByteSize";
+import { useMemo } from "react";
 
 import { useFormContext } from "react-hook-form";
 import type { ComposerFormInputs } from "src/types";
@@ -9,6 +11,15 @@ export const Subject: React.FC = () => {
 
   const value = getValues()["subject"];
   watch(["subject"]);
+  const size = useMemo(() => getByteSize(value), [value]);
+
+  const validate = (value: string) => {
+    const size = getByteSize(value);
+    if (size > MAXIMUM_MAIL_SUBJECT_LENGTH) {
+      return "Your subject exceeds the maximum size";
+    }
+    return !0;
+  };
   return (
     <Input as={Box} position={"relative"} w="100%" p={0}>
       <Input
@@ -21,10 +32,11 @@ export const Subject: React.FC = () => {
         bg="transparent !important"
         variant={"secondary"}
         {...register("subject", {
-          maxLength: {
-            value: MAXIMUM_MAIL_SUBJECT_LENGTH,
-            message: `Maximum length should be ${MAXIMUM_MAIL_SUBJECT_LENGTH}`,
-          },
+          // maxLength: {
+          //   value: MAXIMUM_MAIL_SUBJECT_LENGTH,
+          //   message: `Maximum length should be ${MAXIMUM_MAIL_SUBJECT_LENGTH}`,
+          // },
+          validate: validate,
         })}
       />
 
@@ -37,7 +49,7 @@ export const Subject: React.FC = () => {
         alignItems={"center"}
         fontSize={13}
       >
-        {value.length}/{MAXIMUM_MAIL_SUBJECT_LENGTH}
+        {size}/{MAXIMUM_MAIL_SUBJECT_LENGTH}
       </Box>
     </Input>
   );
